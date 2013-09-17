ac02a1735b3a760f2e21b47516601acb4a968609
vishnu <vishnupadmanabhan@ymail.com>
2013-09-17 12:28:43 +0530

    Libraries included

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 ==================================================================================
 
 
-Starting as a small JavaScript project incorporating fundamentals of JavaScript Development. May evolve into something awesome later.
+Starting as a small JavaScript project incorporating fundamentals of JavaScript 
+Development. May evolve into something awesome later.
 
 
 ----------------------------------------------------------------------------------
--- js/script.js
+++ js/script.js
@@ -5,17 +5,32 @@ var buttons = document.getElementsByTagName('button');
 
 var buttonClick = function() {
         var className = this.innerHTML.toLowerCase();
-
         document.body.className = className;
     };
 
-for ( var i=0, len = buttons.length; i < len; i++ ) {
-    
+for ( var i=0, len = buttons.length; i < len; i++ ) {    
     buttons[i].addEventListner('click', buttonClick,  )
 }
     
 }());
 
-function callBack() {
-    var call = document.getElementById
-}
+var addEvent = function (el, type, fn) {
+    if (typeof addEventListener !== "undefined") {
+        el.addEventListener(type, fn, false);
+    } else if (typeof attachEvent !== "undefined") {
+        el.attachEvent("on" + type, fn);
+    } else {
+        el["on" + type] = fn;
+    }
+};
+
+
+var removeEvent = function (el, type, fn) {
+    if (typeof removeEventListener !== "undefined") {
+        el.removeEventListener(type, fn, false);
+    } else if (typeof attachEvent !== "undefined") {
+        el.removeEvent("on" + type, fn);
+    } else {
+        el["on" + type] = null;
+    }
+};
